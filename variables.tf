7942f493b4bdac297635ef89d6ca1b144159bedc
InZeinly <tuzar11@hotmail.com>
2022-09-27 21:10:45 +0300

    upd descr

--- variables.tf
+++ variables.tf
@@ -4,13 +4,16 @@ variable "aws_region" {
 }
 
 variable "vpc-cidr" {
+    description = "asd"
     cidr_block = "172.31.0.0/16"
 }
 
 variable "public-sub-cidr" {
+    description = "asd1"
     cidr_block = "172.31.0.0/24"
 }
 
 variable "private-sub-cidr" {
+    description = "asd2"
     cidr_block = "172.31.1.0/24"
 }
