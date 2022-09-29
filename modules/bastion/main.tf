38467ace5ff2757fff23e4ffa060d19bf6e2916e
InZeinly <tuzar11@hotmail.com>
2022-09-29 08:44:56 +0300

    try fix

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -19,25 +19,30 @@ data "template_file" "user_data" {
 }
 
 resource "aws_security_group" "bastion_sg" {
-    name = "bastion security group"
-    description = "Allow trafic"
-    vpc_id = var.vpc-cidr.id
-
-    ingress = {
-      cidr_blocks = [ "0.0.0.0/0" ]
-      from_port = 22
-      protocol = "tcp"
-      to_port = 22
-    } 
-
-    egress = {
-      cidr_blocks = [ "0.0.0.0/0" ]
-      from_port = 0
-      protocol = "-1"
-      to_port = 0
+  vpc_id = var.vpc_id
+  name   = "bastion security group"
+  dynamic "ingress" {
+    for_each = var.sg_bas_ingress_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
-
-    tags = {
-      Name = "bastion security group"
-    }
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "bastion security group"
+  }
 }
