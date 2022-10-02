f7e18a7789d9cb67738afddd9f6f77f32861752b
InZeinly <tuzar11@hotmail.com>
2022-10-02 10:42:46 +0300

    try fix

--- sc_group_ssh.tf
+++ sc_group_ssh.tf
@@ -19,23 +19,23 @@ resource "aws_security_group" "ssh" {
     }
 }
 
-resource "aws_security_group" "bastion_sg" {
-    name = "SC for Bastion"
-    description = "bastion sg"
-    vpc_id = aws_vpc.vpc1.id
+# resource "aws_security_group" "bastion_sg" {
+#     name = "SC for Bastion"
+#     description = "bastion sg"
+#     vpc_id = aws_vpc.vpc1.id
 
-    ingress = {
-      cidr_blocks = [ "0.0.0.0/0" ]
-      description = "some"
-      from_port = 22
-      protocol = "tcp"
-      to_port = 22
-    }
+#     ingress = {
+#       cidr_blocks = [ "0.0.0.0/0" ]
+#       description = "some"
+#       from_port = 22
+#       protocol = "tcp"
+#       to_port = 22
+#     }
 
-    egress = {
-      cidr_blocks = [ "0.0.0.0/0" ]
-      from_port = 0
-      protocol = "-1"
-      to_port = 0
-    }
-}
+#     egress = {
+#       cidr_blocks = [ "0.0.0.0/0" ]
+#       from_port = 0
+#       protocol = "-1"
+#       to_port = 0
+#     }
+# }
