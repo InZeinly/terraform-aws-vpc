62b85503762b69b4da76cedd4189afa10cc277b8
InZeinly <tuzar11@hotmail.com>
2022-10-03 19:07:12 +0300

    try fix

--- main.tf
+++ main.tf
@@ -1,37 +1,40 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-
-
-#       version = "~> 3.27"
-#     }
-#   }
-
-
-
-#   # required_version = ">= 0.14.9"
-
-# }
-
-# provider "aws" {
-#   region                = var.aws_region
-# }
-
-# provider "aws" {
-#   region = "eu-central-1"
-# }
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+
+
+      version = "~> 3.27"
     }
   }
+
+
+
+  # required_version = ">= 0.14.9"
+
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = var.aws_region
+provider aws {
+  region                = var.aws_region
 }
+
+
+#test 2
+# provider "aws" {
+#   region = "eu-central-1"
+# }
+
+#test 3
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
+#   }
+# }
+
+# # Configure the AWS Provider
+# provider "aws" {
+#   region = var.aws_region
+# }
