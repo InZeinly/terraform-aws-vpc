68913fe805c8eb6cff4df4d3a793cbcc612be950
InZeinly <tuzar11@hotmail.com>
2022-10-03 18:38:08 +0300

    try fix

--- main.tf
+++ main.tf
@@ -18,6 +18,20 @@
 #   region                = var.aws_region
 # }
 
+# provider "aws" {
+#   region = "eu-central-1"
+# }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region = "eu-central-1"
 }
