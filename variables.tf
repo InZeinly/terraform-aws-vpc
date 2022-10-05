8f4456ea4a633b43141a07abdd99bc051390c33a
InZeinly <tuzar11@hotmail.com>
2022-10-05 22:56:07 +0300

    try

--- variables.tf
+++ variables.tf
@@ -75,18 +75,18 @@ variable "ports-private" {
   type = map(object({
     port = number
     protocol = string
-    cidr_blocks = var.cidr-sg
+    cidr_blocks = list(string)
   }))
   default = {
     "80" = {
       port = 80
       protocol = "tcp"
-      cidr_blocks = var.cidr-sg
+      cidr_blocks = ["0.0.0.0/0"]
     }
     "22" = {
       port = 22
       protocol = "tcp"
-      cidr_blocks = var.cidr-sg
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
 }
