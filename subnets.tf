03c773f0f9e007a846f36e04a3fe320b8b9166d6
InZeinly <tuzar11@hotmail.com>
2022-10-05 22:58:08 +0300

    try

--- subnets.tf
+++ subnets.tf
@@ -30,28 +30,30 @@ resource "aws_subnet" "private-subnet" {
   name = each.value
 }
 
-resource "aws_subnet" "publicsubnet" {
-    vpc_id = aws_vpc.vpc1.id
-    cidr_block = var.public-sub-cidr
-    map_public_ip_on_launch = true
-    tags = {
-      Name = "public-1"
-    }
+# public subnets
 
-    depends_on = [
-      aws_vpc.vpc1
-    ]
-}
+# resource "aws_subnet" "publicsubnet" {
+#     vpc_id = aws_vpc.vpc1.id
+#     cidr_block = var.public-sub-cidr
+#     map_public_ip_on_launch = true
+#     tags = {
+#       Name = "public-1"
+#     }
 
-resource "aws_subnet" "publicsubnet2" {
-    vpc_id = aws_vpc.vpc1.id
-    cidr_block = var.public-sub-cidr2
+#     depends_on = [
+#       aws_vpc.vpc1
+#     ]
+# }
 
-    tags = {
-      Name = "public-2"
-    }
+# resource "aws_subnet" "publicsubnet2" {
+#     vpc_id = aws_vpc.vpc1.id
+#     cidr_block = var.public-sub-cidr2
 
-    depends_on = [
-      aws_vpc.vpc1
-    ]
-}
+#     tags = {
+#       Name = "public-2"
+#     }
+
+#     depends_on = [
+#       aws_vpc.vpc1
+#     ]
+# }
