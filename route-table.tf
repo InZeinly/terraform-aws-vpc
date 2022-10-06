7cfec1cb60d70f29257e065774be31b3d0471770
InZeinly <tuzar11@hotmail.com>
2022-10-06 08:16:06 +0300

    try

--- route-table.tf
+++ route-table.tf
@@ -11,7 +11,7 @@
  }
 
  resource "aws_route_table" "publicRT" {
-  vpc_id =  aws_vpc.vpc.id
+  for_each = aws_vpc.vpc
      route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id[each.key]
