f0d7edbe760c5a505b35b8c63af012dd09492843
InZeinly <tuzar11@hotmail.com>
2022-10-07 08:24:45 +0300

    try

--- route-table.tf
+++ route-table.tf
@@ -1,6 +1,6 @@
 
  resource "aws_route_table" "privateRT" {
-      for_for_each = aws_vpc.vpc
+      for_each = aws_vpc.vpc
       vpc_id = each.value.id
         route {
         cidr_block = var.cidr-open
